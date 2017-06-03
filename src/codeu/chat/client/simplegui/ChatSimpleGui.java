9c360d1613453203156b4897f2948209e7d449e1
thaonguyen19 <thachthao2605@gmail.com>
2017-06-02 22:06:00 -0700

    renamed to ChatBrat

--- src/codeu/chat/client/simplegui/ChatSimpleGui.java
+++ src/codeu/chat/client/simplegui/ChatSimpleGui.java
@@ -63,7 +63,7 @@ public final class ChatSimpleGui {
 
     // Outermost frame.
     // NOTE: may have tweak size, or place in scrollable panel.
-    mainFrame = new JFrame("Chat");
+    mainFrame = new JFrame("ChatBrat");
     mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     mainFrame.setSize(790, 450);
 
